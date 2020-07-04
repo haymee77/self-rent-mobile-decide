02015e23ac8b44ef1342ef3886720b4472ee1817
박혜미 <haymee@naturemobility.com>
2020-07-04 16:33:06 +0900

    env test

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 function App() {
   return (
-    <div className='App'>
-      {process.env.NODE_ENV} / {process.env.T}
+    <div>
+      <h3>.env test</h3>
+      <h5>ENV_TEST: {process.env.ENV_TEST}</h5>
+      <h5>REACT_APP_ENV_TEST: {process.env.REACT_APP_ENV_TEST}</h5>
+      <h5>NODE_ENV: {process.env.NODE_ENV}</h5>
     </div>
   );
 }
