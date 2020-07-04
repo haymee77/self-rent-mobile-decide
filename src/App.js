1768f9a940f94a8797b541051392ed317b945bef
haymee <hmpark0502@gmail.com>
2020-07-04 23:53:30 +0900

    route test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h3>.env test</h3>
-      <h5>ENV_TEST: {process.env.ENV_TEST}</h5>
-      <h5>REACT_APP_ENV_TEST: {process.env.REACT_APP_ENV_TEST}</h5>
-      <h5>NODE_ENV: {process.env.NODE_ENV}</h5>
-    </div>
+    <Router>
+      <Fragment>
+        <Route exact path='/' component={Home} />
+      </Fragment>
+    </Router>
   );
 }
 
